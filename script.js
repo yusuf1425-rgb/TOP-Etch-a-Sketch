937bfcfb2eb652c7161861d14f8a28f8f102d6e8
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-16 11:46:44 +0530

    Add functionality to change color of divs on mouse hover

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const container = document.querySelector("#container");
+
 const grid = document.createElement("div");
 grid.classList.add("grid");
+
 const gridValue = 64;
 const numOfCells = gridValue * gridValue;
-for(let i = 1; i <= numOfCells; i++){
-    const cells = document.createElement("div");
-    cells.classList.add("cells")
-    cells.style.width = `${900/ gridValue}px`;
-    cells.style.height = `${900 / gridValue}px`;
-    grid.appendChild(cells);
-}
 
+for(let i = 1; i <= numOfCells; i++){
+    const cell = document.createElement("div");
+    cell.classList.add("cell")
+
+    cell.style.width = `${900/ gridValue}px`;
+    cell.style.height = `${900 / gridValue}px`;
+
+    grid.appendChild(cell);
+}
 container.appendChild(grid);
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";
+    });
+}); 
+
+
