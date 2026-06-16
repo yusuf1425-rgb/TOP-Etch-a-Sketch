0841d8fe29a1283a493e7294025ee9c31069fdd2
Yusuf <236015542+yusuf1425-rgb@users.noreply.github.com>
2026-06-16 11:22:46 +0530

    Add logic to handle different grid sizes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector("#container");
 const grid = document.createElement("div");
 grid.classList.add("grid");
-
-for(let i = 1; i <= 16; i++){
+const gridValue = 64;
+const numOfCells = gridValue * gridValue;
+for(let i = 1; i <= numOfCells; i++){
     const cells = document.createElement("div");
     cells.classList.add("cells")
+    cells.style.width = `${900/ gridValue}px`;
+    cells.style.height = `${900 / gridValue}px`;
     grid.appendChild(cells);
 }
 
